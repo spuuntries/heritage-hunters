f640748fd402a7838eddb5d253713041ea1b0aaf
rasyadlubisdev <rasyadloebisa@gmail.com>
2024-06-24 21:55:38 +0700

    feat: add username text input

--- code/main.py
+++ code/main.py
@@ -6,7 +6,7 @@ from button import Button
 
 class Game:
     def __init__(self):
-        self.state = "menu"
+        self.state = "input_name"
 
         # general setup
         pygame.init()
@@ -14,6 +14,15 @@ class Game:
         pygame.display.set_caption("Zelda")
         self.clock = pygame.time.Clock()
         self.level = Level()
+
+        # input name setup
+        self.user_name = ""
+        self.font = pygame.font.Font(None, 50)
+        self.input_box = pygame.Rect(400, 300, 400, 50)
+        self.color_inactive = pygame.Color('lightskyblue3')
+        self.color_active = pygame.Color('dodgerblue2')
+        self.color = self.color_inactive
+        self.active = False
 
         # sound
         # main_sound = pygame.mixer.Sound("../audio/main.ogg")
@@ -37,11 +46,44 @@ class Game:
             pygame.display.update()
             self.clock.tick(FPS)
 
+    def input_name(self):
+        while self.state == "input_name":
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.input_box.collidepoint(event.pos):
+                        self.active = not self.active
+                    else:
+                        self.active = False
+                    self.color = self.color_active if self.active else self.color_inactive
+                if event.type == pygame.KEYDOWN:
+                    if self.active:
+                        if event.key == pygame.K_RETURN:
+                            # generate pass nya nanti di sini kah?
+                            self.state = "menu"
+                        elif event.key == pygame.K_BACKSPACE:
+                            self.user_name = self.user_name[:-1]
+                        else:
+                            self.user_name += event.unicode
+
+            self.screen.fill((30, 30, 30))
+            txt_surface = self.font.render(self.user_name, True, self.color)
+            width = max(400, txt_surface.get_width() + 10)
+            self.input_box.w = width
+            self.screen.blit(txt_surface, (self.input_box.x + 5, self.input_box.y + 5))
+            pygame.draw.rect(self.screen, self.color, self.input_box, 2)
+            pygame.display.flip()
+            self.clock.tick(30)
+
     def main_menu(self):
         while self.state == "menu":
             MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-            MENU_TEXT = self.get_font(100).render("MAIN MENU", True, "#b68f40")
+            self.screen.fill("black")
+
+            MENU_TEXT = self.get_font(100).render(f"Hello {self.user_name}", True, "#b68f40")
             MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
 
             PLAY_BUTTON = Button(
@@ -88,4 +130,5 @@ class Game:
 
 if __name__ == "__main__":
     game = Game()
+    game.input_name()
     game.main_menu()
